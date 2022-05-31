88e2ac5f6b82d85fc6e040c591b12df7f1f4b9c8
David Vera <indigodavid@gmail.com>
2022-05-31 15:30:07 -0500

    Add basic html template and fontawesome as module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import _ from 'lodash';
+import '@fortawesome/fontawesome-free/';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
 
 function component() {
