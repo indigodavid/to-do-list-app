680df26f78306878b80669127dfee1cc026a1f13
David Vera <indigodavid@gmail.com>
2022-06-01 15:25:29 -0500

    Add input element to edit each task

--- src/modules/create-li.js
+++ src/modules/create-li.js
@@ -2,36 +2,65 @@ const taskList = document.getElementById('task-list');
 
 const createLi = (task) => {
   const li = document.createElement('li');
-  const input = document.createElement('input');
+  const checkbox = document.createElement('input');
   const div = document.createElement('div');
   const button = document.createElement('button');
+  const textInput = document.createElement('input');
 
+  // Set List element id and class
   li.setAttribute('id', `task${task.index}`);
   li.classList.add('task');
+  
+  // Set checkbox attributes
+  checkbox.checked = task.completed;
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.setAttribute('name', `check${task.index}`);
+  checkbox.setAttribute('id', `check${task.index}`);
 
-  input.checked = task.completed;
-  input.setAttribute('type', 'checkbox');
-  input.setAttribute('name', `check${task.index}`);
-  input.setAttribute('id', `check${task.index}`);
+  // Set text input attributes
+  textInput.classList.add('edit');
+  textInput.setAttribute('type', 'text');
+  textInput.setAttribute('name', `text${task.index}`);
+  textInput.setAttribute('id', `text${task.index}`);
+  textInput.value = task.description;
 
+  // Set div classes and content
   div.classList.add('text');
   div.innerHTML = task.description;
-  if (input.checked) {
+  if (checkbox.checked) {
     div.classList.add('done');
   }
 
   button.classList.add('options');
   button.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
 
-  li.appendChild(input);
+  li.appendChild(checkbox);
   li.appendChild(div);
+  li.appendChild(textInput);
   li.appendChild(button);
 
   taskList.appendChild(li);
 
-  input.addEventListener('change', () => {
+  checkbox.addEventListener('change', () => {
     div.classList.toggle('done');
   });
+
+  div.addEventListener('click',() => {
+    div.style.display = 'none';
+    textInput.style.display = 'inherit';
+  })
+
+  textInput.addEventListener('change', () => {
+    div.innerHTML = textInput.value;
+    // edit local storage
+    div.style.display = 'inherit';
+    textInput.style.display = 'none';
+  })
+
+  textInput.addEventListener('focusout', () => {
+    div.style.display = 'inherit';
+    textInput.style.display = 'none';
+  })
 }
 
 export default createLi;
