fcce928c164a88e408470ec2fff2970578dc4805
David Vera <indigodavid@gmail.com>
2022-06-01 12:39:24 -0500

    Refactor entry point with new functionalities

--- src/index.js
+++ src/index.js
@@ -1,57 +1,16 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
+import Task from './modules/task.js'
+import createLi from './modules/create-li';
+import addTaskEvent from './modules/add-task';
 
 const toDoTasks = [
-  {
-    description: 'Do the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Study JS',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Correct project',
-    completed: false,
-    index: 3,
-  },
+    new Task('Do the dishes', 0),
+    new Task('Study JS', 1),
+    new Task('Complete the project', 2),
 ];
 
-const taskList = document.getElementById('task-list');
+toDoTasks.forEach((task) => createLi(task));
 
-toDoTasks.forEach((task) => {
-  const li = document.createElement('li');
-  const input = document.createElement('input');
-  const div = document.createElement('div');
-  const button = document.createElement('button');
-
-  li.setAttribute('id', `task${task.index}`);
-  li.classList.add('task');
-
-  input.checked = task.completed;
-  input.setAttribute('type', 'checkbox');
-  input.setAttribute('name', `check${task.index}`);
-  input.setAttribute('id', `check${task.index}`);
-
-  div.classList.add('text');
-  div.innerHTML = task.description;
-  if (input.checked) {
-    div.classList.add('done');
-  }
-
-  button.classList.add('options');
-  button.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
-
-  li.appendChild(input);
-  li.appendChild(div);
-  li.appendChild(button);
-
-  taskList.appendChild(li);
-
-  input.addEventListener('change', () => {
-    div.classList.toggle('done');
-  });
-});
+addTaskEvent();
